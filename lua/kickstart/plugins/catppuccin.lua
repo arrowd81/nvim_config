d6e01f37ed26b761411e5588c6faad92c61837c2
arrow <arashd1381@gmail.com>
2024-05-11 22:28:25 +0330

    changed colors for better visibility

--- lua/kickstart/plugins/catppuccin.lua
+++ lua/kickstart/plugins/catppuccin.lua
@@ -7,6 +7,15 @@ return {
       -- setup must be called before loading
       require('catppuccin').setup {
         transparent_background = true,
+        custom_highlights = function()
+          local visibale_color = '#8c6c89'
+          return {
+            LineNr = { fg = visibale_color },
+            StatusLineNC = { fg = visibale_color },
+            EndOfBuffer = { fg = visibale_color },
+            Comment = { fg = '#6fad8d' },
+          }
+        end,
       }
       vim.cmd.colorscheme 'catppuccin'
     end,
