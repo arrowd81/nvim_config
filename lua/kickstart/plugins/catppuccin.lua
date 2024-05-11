a345ea8bae3bb4434bb873c75284e48cae1c0733
arrow <arashd1381@gmail.com>
2024-05-11 19:59:13 +0330

    added transparent background to catppuccin theme

--- lua/kickstart/plugins/catppuccin.lua
+++ lua/kickstart/plugins/catppuccin.lua
@@ -5,6 +5,9 @@ return {
     priority = 1000,
     config = function()
       -- setup must be called before loading
+      require('catppuccin').setup {
+        transparent_background = true,
+      }
       vim.cmd.colorscheme 'catppuccin'
     end,
   },
